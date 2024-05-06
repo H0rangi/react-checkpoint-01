2699f8582fd5ed762e5ae3ed16b780ee5ce73ecb
H0rangi <formation0451@hotmail.com>
2024-05-06 12:21:55 +0100

    react

---
+++ src/product.js
@@ -0,0 +1,7 @@
+export const product = {
+    name: "AMD Ryzen™ 7 5800X3D Gaming Processor",
+    price: 36800,
+    description: "The AMD Ryzen™ 7 5800X3D unleashes an average of 15% more gaming performance with AMD 3D V-Cache™ technology.¹",
+    imageURL: "https://www.amd.com/content/dam/amd/en/images/products/processors/ryzen/2505503-ryzen-7-5800x3d.jpg"
+};
+
